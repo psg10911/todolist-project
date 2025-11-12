0343cd0a91f6815d415c04ab8209cd18c667f8ea
heeseo0831 <heeseo0831@kyonggi.ac.kr>
2025-11-12 17:59:59 +0900

    Add files via upload

--- Todo/Todo/Task.java
+++ Todo/Todo/Task.java
@@ -11,6 +11,8 @@ public class Task {
     private String startDate;
     private String endDate;
     private boolean completed;
+    private String priority; // 중요도: HIGH / MEDIUM / LOW
+
 
     // TaskDialog 생성자 -insert용
     public Task(String title, String content, String startDate, String endDate) {
@@ -20,6 +22,7 @@ public class Task {
         this.startDate = startDate;
         this.endDate = endDate;
         this.completed = false; // 새로 추가된 할 일은 항상 '미완료'
+        this.priority = priority;
         
     }
     // select용
@@ -32,6 +35,7 @@ public class Task {
     this.startDate = startDate;
     this.endDate = endDate;
     this.completed = completed;
+    this.priority = priority;
 }
 
     // Getters
@@ -42,6 +46,7 @@ public class Task {
     public String getStartDate() { return startDate; }
     public String getEndDate() { return endDate; }
     public boolean isCompleted() { return completed; }
+    public String getPriority() { return priority; }
 
     // Setters(ui에서 수정될수있는 값들)
     public void setUserId(String userId) { this.userId = userId; }
@@ -49,6 +54,7 @@ public class Task {
     public void setStartDate(String startDate) { this.startDate = startDate; }
     public void setEndDate(String endDate) { this.endDate = endDate; }
     public void setCompleted(boolean completed) { this.completed = completed; }
+    public void setPriority(String priority) { this.priority = priority; }
     /**
      * [수업 자료] JTable의 tableModel.addRow() 에 넣기 적합한
      * Object 배열 형태로 데이터를 변환합니다.
@@ -60,7 +66,8 @@ public class Task {
             completed, // 2번: 완료 여부 (Boolean)
             title,     // 3번: 제목 (String)
             startDate, // 4번: 시작일 (String)
-            endDate    // 5번: 종료일 (String)
+            endDate,    // 5번: 종료일 (String)
+            priority    // 6번: 중요도 (String) 
         };
     }
 }
